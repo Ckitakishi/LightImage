ac8d26222e50816d2c06d84c575622482ac5f8e9
Ckitakishi <chenyuhan43@gmail.com>
2015-04-12 23:28:30 +0800

    结构

--- app.js
+++ app.js
@@ -11,8 +11,17 @@ app.engine('html', require('ejs').renderFile); // .htmlはejsを利用
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use(favicon(path.join(__dirname, 'public', 'media', 'images', 'favicon.ico')));
 
+// routers  --write here for a while
 app.get('/', function (req, res) {
   res.render("index");
+});
+
+app.get('/photo', function (req, res) {
+  res.render("photo/photo")
+});
+
+app.get('/paint', function (req, res) {
+  res.render("paint/paint")
 });
 
 app.listen(3003, function () {
