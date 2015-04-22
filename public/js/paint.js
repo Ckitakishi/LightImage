0e5b17479dd75b34b911947203a665c8ef60df12
Ckitakishi <chenyuhan43@gmail.com>
2015-04-22 17:44:16 +0800

    upload of paint

--- public/js/paint.js
+++ public/js/paint.js
@@ -4,22 +4,24 @@ new Vue({
     isLoad: false,
     canvasShow: false,
     curPoint: {},
-    ctx: ""
+    ctx: "",
+    drawing: false,
+    aborted: false,
+    paintBtn: "Start Paint"
   },
   methods: {
     paint: function () {
       this.isLoad = true;
       this.canvasShow = true;
+      this.paintBtn = "Clear";
 
       var canvas = document.getElementById("myCanvas");
-      var ctx = canvas.getContext("2d");
-      this.ctx = ctx;
+      this.ctx = canvas.getContext("2d");
     },
-    drawPath: function(e) {
+    drawPathStart: function (e) {
       var self = this;
-      self.ctx.fillStyle = 'red';
-      px = e.layerX;
-      py = e.layerY;
+      var px = e.layerX;
+      var py = e.layerY;
 
       function isEmpty(obj)
       {
@@ -34,15 +36,57 @@ new Vue({
       }
 
       if (isEmpty(self.curPoint)) {
-        self.curPoint.x = px;
-        self.curPoint.y = py;
-        self.ctx.beginPath();
-      } else {
+        var canvas = document.getElementById("myCanvas");
+
+        var height = canvas.offsetHeight;
+        var width = canvas.offsetWidth;
+
+        canvas.height = height;
+        canvas.width = width;
+        self.ctx = canvas.getContext("2d");
+      }
+
+      self.curPoint.x = px;
+      self.curPoint.y = py;
+      self.ctx.beginPath();
+
+      self.drawing = true;
+    },
+    drawPath: function(e) {
+      var self = this;
+      var px, py;
+
+      if (self.drawing) {
+        self.ctx.fillStyle = 'red';
+        px = e.layerX;
+        py = e.layerY;
+
         self.ctx.moveTo(self.curPoint.x, self.curPoint.y);
         self.ctx.lineTo(px, py);
         self.ctx.stroke();
+        self.ctx.closePath();
         self.curPoint.x = px;
         self.curPoint.y = py;
+      }
+    },
+    drawPathEnd: function() {
+      this.drawing = false;
+    },
+    drawPathAbort: function () {
+      this.drawing = false;
+      this.aborted = true;
+    },
+    drawPathContinue: function (e) {
+      var self = this;
+
+      if (self.aborted && !self.drawing) {
+        var px = e.layerX;
+        var py = e.layerY;
+
+        self.curPoint.x = px;
+        self.curPoint.y = py;
+        self.ctx.beginPath();
+        self.drawing = true;
       }
     }
   }
